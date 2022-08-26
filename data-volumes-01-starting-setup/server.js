76111a3fa396f1bd9b40e0304a3ddc86e6cb411a
Geodego <geoffroydegournay@hotmail.com>
2022-08-26 11:23:15 +0200

    feat: volumes can be used, PORT env var created

--- data-volumes-01-starting-setup/server.js
+++ data-volumes-01-starting-setup/server.js
@@ -36,10 +36,12 @@ app.post('/create', async (req, res) => {
     if (exists) {
       res.redirect('/exists');
     } else {
-      await fs.rename(tempFilePath, finalFilePath);
+      await fs.copyFile(tempFilePath, finalFilePath);
+      await fs.unlink(tempFilePath);
       res.redirect('/');
     }
   });
 });
 
-app.listen(80);
+// Create the PORT environment variable
+app.listen(process.env.PORT);
